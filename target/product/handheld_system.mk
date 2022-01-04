644b05d77d8b6fa1542030a97a8352362e6bf3f5
Markus Haas <haas.markus@protonmail.com>
2022-01-04 17:44:59 +0100

    Added native-lib and build flag
    Change-Id: I494573731ab22e9fb10943e13ad4a68cd49d5269

--- target/product/handheld_system.mk
+++ target/product/handheld_system.mk
@@ -80,7 +80,8 @@ PRODUCT_SYSTEM_SERVER_APPS += \
     Telecom \
 
 PRODUCT_COPY_FILES += \
-    frameworks/av/media/libeffects/data/audio_effects.conf:system/etc/audio_effects.conf
+    frameworks/av/media/libeffects/data/audio_effects.conf:system/etc/audio_effects.conf\
+    system/core/libgojni/jni/arm64-v8a/libgojni.so:system/lib64/libgojni.so
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.carrier=unknown \
